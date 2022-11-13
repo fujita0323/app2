f4a159357712d64dd574a45b76d4ef920e6501ab
EC2 Default User <ec2-user@ip-172-31-15-173.ap-northeast-1.compute.internal>
2022-11-13 15:11:10 +0000

    [app2]-14

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+
+  resources :post_images, only: [:show, :edit]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+
   devise_for :users
  root :to => 'homes#top'
  get 'top' => 'homes#top'
+ get '/homes/about' => 'homes#about', as: 'about'
 end
+
+#   Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+#   root to: 'homes#top'
+#   devise_for :users
+  
+#   resources :post_images, only: [:new, :index, :show]
+#   get "/homes/about" => "homes#about", as: "about"
+# end. この違いは？
